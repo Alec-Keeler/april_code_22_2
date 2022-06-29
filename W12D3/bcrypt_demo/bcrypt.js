2765d03053b1bdbda026ce5822ecf022f7577f11
Alec Keeler <alec.keeler@gmail.com>
2022-06-29 11:40:42 -0500

    w12d3 done

--- W12D3/bcrypt_demo/bcrypt.js
+++ W12D3/bcrypt_demo/bcrypt.js
@@ -1,1 +1,10 @@
 const bcrypt = require('bcryptjs');
+
+function hashPassword(password) {
+    const hash = bcrypt.hashSync(password, 12)  //password + salt
+    console.log(hash)
+}
+
+hashPassword('password')
+
+// $2a$10$pcgsj4W4VqTEwNGic/gSg.Nf/T8xwL37e4XjB2vKiSZhIPjo6.s.W
